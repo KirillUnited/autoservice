06c521941645f4217c4d36e1fc1d8b74a12a0844
k.dubov <example@example.com>
2023-03-23 13:34:36 +0300

    install tailwind

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,14 +3,10 @@ import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => {
   return (
-    <header id="site-header" className="custom-header effect-three clr" role="banner">
-      <div id="site-header-inner" className="clr">
-        <div className="elementor elementor-8">
-          <div className="elementor-section-wrap">
-            {children}
-          </div>
-        </div>
-      </div>
+    <header id="site-header" className="header" role="banner">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </header>
   )
 }
