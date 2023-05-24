398a8338f6b00eda548bd967c079c170c909522f
k.dubov <example@example.com>
2023-05-24 16:43:07 +0300

    Fix Button

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,8 +1,10 @@
 import * as React from "react";
 import * as styles from "./button.module.scss";
 
-export default function Button({type="button", className="", label="", ...rest}){
+const Button = React.forwardRef(({ type = "button", className = "", label = "", ...rest }, ref) => {
     return (
-        <button type={type} className={`${styles.btn} ${styles.btnPrimary} ${className}`} {...rest}>{label}</button>
+        <button type={type} className={`${styles.btn} ${styles.btnPrimary} ${className}`} {...rest} ref={ref}>{label}</button>
     )
-}
+});
+
+export default Button;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 import logo from "../../images/logo.png"
 import * as styles from "./header.module.scss"
-import HeaderModal from "./HeaderModal";
+import Modal from "../modal/Modal";
 
 const menu = {
   'Автосервис РиаМоторс': null,
@@ -46,7 +46,7 @@ const Header = ({ siteTitle }) => {
             </div>
           </div>
           <div className={styles.cta}>
-            <HeaderModal />
+            <Modal />
             <p className="mb-0 font-semibold"><strong className="font-semibold">Пон. - Пят. 8.00 - 20.00</strong></p>
           </div>
           <div className={styles.contacts}>
