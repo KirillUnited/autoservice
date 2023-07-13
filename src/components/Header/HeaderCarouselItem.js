f934c6155ce71bbb49d9cddd4f3961c7415d28f1
k.dubov <example@example.com>
2023-07-13 07:16:35 +0300

    Complete header auto models carousel

--- src/components/Header/HeaderCarouselItem.js
+++ src/components/Header/HeaderCarouselItem.js
@@ -1,11 +1,12 @@
+import { Link } from 'gatsby';
 import React from 'react'
 
 export default function HeaderCarouselItem({ data }) {
     const { logo = "", link = "" } = data;
-console.log(logo)
+
     return (
-        <a href={link} className='relative block h-full'>
+        <Link to={link} className='relative block h-full'>
             <div className='bg-contain bg-no-repeat bg-center h-full hover:scale-125 transition-all' style={{ backgroundImage: `url(${logo})` }} />
-        </a>
+        </Link>
     )
 }
