ef61c10eb4e340688c94ac22511d7f6b23fadfc7
k.dubov <example@example.com>
2023-07-27 14:33:46 +0300

    Adjust button

--- src/components/Section1/index.js
+++ src/components/Section1/index.js
@@ -21,10 +21,12 @@ export default function Section1({ childern }) {
     const { frontmatter, html } = data?.allMarkdownRemark?.nodes[0] || {};
 
     return (
-        <section className="container text-center py-10">
-            <h1 className="text-4xl heading mb-5">{frontmatter.title}</h1>
-            <h3 className="text-lg mb-5">{frontmatter.desc}</h3>
-            <div className='text-sm space-y-4' dangerouslySetInnerHTML={{__html: html}}></div>
+        <section className="py-10">
+            <div className='container text-center'>
+              <h1 className="text-4xl heading mb-5">{frontmatter.title}</h1>
+              <h3 className="text-lg mb-5">{frontmatter.desc}</h3>
+              <div className='text-sm space-y-4' dangerouslySetInnerHTML={{__html: html}}></div>
+            </div>
         </section>
     )
 }
